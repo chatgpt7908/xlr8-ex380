cffbbbdaf3f77a6cce5f4d651d4a1fbcba9003df
Sachin <sachin@example.com>
2025-12-16 01:49:50 -0500

    fixed q3 run deleted

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-VELERO="oc -n openshift-adp exec deployment/velero -c velero -- ./velero"
-
-$VELERO delete  schedule --all --confirm
-$VELERO delete  backup --all --confirm
-$VELERO delete  restore --all --confirm
